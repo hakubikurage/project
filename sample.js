7601bbe083c71719aa8652fdffd5446ea0d4923e
hakubikurage <hakubi.kurata@gmail.com>
2021-12-30 23:01:44 +0900

    head共通化途中＆header ポジション(高さｚ)追加　スライダー部分のポジション調整できず終了

---
+++ sample.js
@@ -0,0 +1,31 @@
+///// ヘッダー共通/////
+'use strict';
+
+{
+    document.head.insertAdjacentHTML('afterbegin' ,`
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,intial-scale=1">
+  <meta name="description" content="ハクビ京都きもの学院プロデュースのきものレンタル＆撮影スタジオ">
+  <link rel="icon" type="image/png" href="images/favicon.png">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.4/css/all.css">
+  <link rel="stylesheet" href="swiper/swiper.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+    `)
+}
+
+// function header(){
+//     // alert("aaa");
+//     $.ajax({
+//         url: "C:\Users\hakubi-sugamo114\Documents\きものスタジオ京WEB作成\project/header.html",
+//         cache: false,
+//         //async: false,
+//         //dataType: 'html',
+//         success: function(html){
+//             document.write(html);
+//         }
+//     });
+// }
+
